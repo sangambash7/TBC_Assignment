a2d15351748e1aaba86bb2d782191564f342665c
Aleksandre Maisuradze <aleksandremaisuradze11@gmail.com>
2024-10-23 22:51:26 +0400

    create product add and delete functional

---
+++ src/app/_Components/ProductsModal/ProductsModal.js
@@ -0,0 +1,53 @@
+import React from 'react'
+
+export default function ProductsModal({
+    isOpen,
+    onClose,
+    title,
+    body,
+    price,
+    setTitle,
+    setBody,
+    setPrice,
+    onSubmit,
+    action,
+}) {
+
+    if (isOpen) {
+        return null
+    }
+
+    return (
+        <div>
+            <div>
+                <span className="close" onClick={onClose}>
+                    &times;
+                </span>
+                <h2>{action === "create" ? "Create New Product" : "Edit Product"}</h2>
+                <div className="post-content">
+                    <input
+                        type="text"
+                        placeholder="Title"
+                        value={title}
+                        onChange={(e) => setTitle(e.target.value)}
+                    />
+                    <input
+                        type='text'
+                        placeholder="Body"
+                        value={body}
+                        onChange={(e) => setBody(e.target.value)}
+                    />
+                    <input
+                        type="number"
+                        placeholder="price"
+                        value={price}
+                        onChange={(e) => setPrice(e.target.value)}
+                    />
+                </div>
+                <button onClick={onSubmit}>
+                    {action === "create" ? "Add Product" : "Save Changes"}
+                </button>
+            </div>
+        </div>
+    )
+}
