d0d2c58956aaacffbbe8c904e2492509b6904f83
lukaabramishvili <92173962+lukaabramishvili@users.noreply.github.com>
2024-10-24 20:27:27 +0400

    Removing Middleware, Adding route group layout

---
+++ src/app/(auth)/layout.js
@@ -0,0 +1,20 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import { useRouter } from "next/navigation";
+import { checkisIsAuthenticated } from "../_Services/authService";
+
+export default function authLayout({ children }) {
+  const [isAuthenticated, setIsAuthenticated] = useState(true);
+  const router = useRouter();
+
+  useEffect(() => {
+    async function getAuthStatus() {
+      const res = await checkisIsAuthenticated();
+      setIsAuthenticated(res);
+    }
+    getAuthStatus();
+  }, []);
+
+  return <div>{isAuthenticated ? children : router.push("/Login")}</div>;
+}
